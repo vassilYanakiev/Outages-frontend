f513b4a0283884763f39801bab322385ff37ac03
Vassil Yanakiev <vsy4@mail.bg>
2020-08-03 07:38:43 +0300

    Updates

---
+++ precache-manifest.f3d7cb83ccee52f5f312b0605dc71d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23b1348781484adb0e26b156b5530e7",
+    "url": "/Outages-frontend/index.html"
+  },
+  {
+    "revision": "745da40b4e905b7503ef",
+    "url": "/Outages-frontend/static/css/2.9e2b3803.chunk.css"
+  },
+  {
+    "revision": "50de2f8bfad656a23589",
+    "url": "/Outages-frontend/static/css/main.f43e7672.chunk.css"
+  },
+  {
+    "revision": "745da40b4e905b7503ef",
+    "url": "/Outages-frontend/static/js/2.c2cd0640.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Outages-frontend/static/js/2.c2cd0640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50de2f8bfad656a23589",
+    "url": "/Outages-frontend/static/js/main.2ea2e3fb.chunk.js"
+  },
+  {
+    "revision": "cfc3302e2a8d105ff181",
+    "url": "/Outages-frontend/static/js/runtime-main.9168774c.js"
+  }
+]);
