a3fe668a5c8ffe563fd0fafe13b7f5054a727b21
Vassil Yanakiev <vsy4@mail.bg>
2020-08-12 16:36:15 +0300

    Updates

---
+++ precache-manifest.2102f5ab498dd649c19268a20c011a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50a0d12d8928799dc47ea7442640963",
+    "url": "/Outages-frontend/index.html"
+  },
+  {
+    "revision": "ef7a11e79cbad4df9128",
+    "url": "/Outages-frontend/static/css/2.9e2b3803.chunk.css"
+  },
+  {
+    "revision": "6b296c15ea07151f8f75",
+    "url": "/Outages-frontend/static/css/main.b864add4.chunk.css"
+  },
+  {
+    "revision": "ef7a11e79cbad4df9128",
+    "url": "/Outages-frontend/static/js/2.9f0e6a72.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Outages-frontend/static/js/2.9f0e6a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b296c15ea07151f8f75",
+    "url": "/Outages-frontend/static/js/main.aa0c2d7e.chunk.js"
+  },
+  {
+    "revision": "cfc3302e2a8d105ff181",
+    "url": "/Outages-frontend/static/js/runtime-main.9168774c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Outages-frontend/precache-manifest.e2b0b7c52cdb54c86bfae30879afa58e.js"
+  "/Outages-frontend/precache-manifest.2102f5ab498dd649c19268a20c011a9a.js"
 );
 
 self.addEventListener('message', (event) => {
